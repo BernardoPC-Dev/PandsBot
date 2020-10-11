0b9376cc09d61df93d8fc197daf3c87282007101
Bernardo Botto <67174320+BernardoPC-Dev@users.noreply.github.com>
2020-10-11 17:47:52 -0300

    Update bot.py

--- src/main/bot.py
+++ src/main/bot.py
@@ -7,4 +7,12 @@ client = commands.Bot(command_prefix = '/')
 async def on_ready():
     print("Eu estou pronta!")
 
+@client.event
+async def on_member_join(member):
+    print(f'{member} entrou no servidor! Divirta-se aqui!')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} saiu do servidor! Espero que ele volte logo...')
+
 client.run('NzY0NjU1OTExNjMzMDI3MDky.X4JbTw._nFs91rqmWBnzEi8tDgIr94LyQc')
