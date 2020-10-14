1fe0f0fd96a23ef44b64b49c029f4735a0f86a5f
Bernardo Botto <67174320+BernardoPC-Dev@users.noreply.github.com>
2020-10-13 22:29:36 -0300

    Add ask command

--- src/bot.py
+++ src/bot.py
@@ -18,7 +18,7 @@ async def roll(ctx, *, n):
     await ctx.send(f'Eu rolei {n} e caiu o número {number}!')
 
 # Clear Command
-
+# Add Perm
 @client.command()
 async def clear(ctx, msgs=1):
     await ctx.channel.parge(limit-msgs)
@@ -30,55 +30,55 @@ async def prefix(ctx):
     await ctx.send(f'Meu prefixo é {client}')
 
 # Say Command
-
+# Add Perm
 @client.command()
 async def say(ctx, arg):
     await ctx.send(f'{arg}')
 
 # Kick Command
-
+# Add Perm
 @client.command()
 async def kick(ctx, member : discord.Member, *, reason=None):
     await member.kick(reason=reason)
     await ctx.send(f"O usuário foi expulso por `{reason}`!")
 
 # Ban Command
-
+# Add Perm
 @client.command()
 async def ban(ctx, member : discord.Member, *, reason=None):
     await member.ban(reason=reason)
     await ctx.send(f"O usuário foi banido por `{reason}`!")
 
 # Unban Command
-
+# Add Perm
 @client.command()
 async def unban(ctx, member : discord.Member):
     await member.unban()
     await ctx.send("O banimento do usuário foi retirado!")
 
 # Create-Channel Command
-
+# Add Perm
 @client.command()
 async def create-channel(ctx, channel):
     await guild.create_text_channel(channel)
     await ctx.send("Canal criado!")
 
 # Nickname Command
-
+# Add Perm
 @client.command()
 async def nickname(ctx, nickname):
     await member.edit(nickname)
     await ctx.send("O apelido foi alterado!")
 
 # Clear-Reactions
-
+# Add Perm
 @client.command()
 async def clear-reactions(ctx, message)
     await message.clear_reactions()
     await ctx.send("As reações da mensagem foram apagadas!")
 
 # Softsay Command
-
+# Add Perm
 @client.command()
 async def softsay(ctx, channel, *, msg):
     await ctx.channel.send(f'{msg}')
@@ -90,6 +90,14 @@ async def softsay(ctx, channel, *, msg):
 async def star(ctx):
     await ctx.send(':star:')
     await ctx.add_reaction(':star:')
+
+# Ask Command
+
+@client.command()
+async def ask(ctx, question):
+    resp = (':+1:', ':-1:')
+    rdm = randint(1, 2)
+    await ctx.send(f'Você perguntou: {question}, e a resposta é: {resp[rdm]}!')
 
 # Run Bot
 
