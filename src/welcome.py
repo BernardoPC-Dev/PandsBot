ccdb62e8c81b64ba26876cb918245023251d6cea
Bernardo Botto <67174320+BernardoPC-Dev@users.noreply.github.com>
2020-10-16 19:12:23 -0300

    Add setwelcome command.

--- src/welcome.py
+++ src/welcome.py
@@ -2,6 +2,10 @@ import discord
 from discord.ext import commands
 import bot
 
-@client.event()
-async def on_member_join(ctx):
-   await ctx.channel.send(f'{msg}')
+@client.command()
+@commands.has_permissons(manage_server=True)
+async def setwelcome(ctx, channel, msg):
+   
+   @client.event()
+   async def on_member_join(ctx):
+      await ctx.channel.send(f'{msg}')
